3e8994ea3e8380864e5439c4a2e5a8c1f4d6fff8
buray <charlieburay@163.com>
2022-05-22 18:42:39 +0800

    labelquery

--- main.go
+++ main.go
@@ -1,54 +1,7 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	etcdCt "go.etcd.io/etcd/client/v3"
-	"log"
-	"time"
-)
-
-var etcdServerList = []string{"http://192.168.123.161:2379", "http://192.168.123.160:2379", "http://192.168.123.162:2379"}
+import "fmt"
 
 func main() {
-	etcdClient, err := etcdCt.New(etcdCt.Config{
-		Endpoints:   etcdServerList,
-		DialTimeout: 5 * time.Second,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer etcdClient.Close()
-
-	for _, i := range etcdServerList {
-		clientStatus, err := etcdClient.Status(context.Background(), i)
-		if err != nil {
-			log.Fatal(err)
-		}
-		println(clientStatus.Version)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	ttl, err := etcdClient.Grant(ctx, 20)
-	if err != nil {
-		log.Fatal(err)
-	}
-	leaseId := ttl.ID
-	fmt.Println("leaseId", leaseId)
-	_, err = etcdClient.Put(ctx, "sample_key2", "sample_value", etcdCt.WithLease(leaseId))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result, err := etcdClient.Get(ctx, "sample_key1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	cancel()
-
-	println(string(result.Kvs[0].Value))
-	println(string(result.Kvs[0].Key))
-
-	fmt.Println("this is man function")
+	fmt.Println("this is golang")
 }
